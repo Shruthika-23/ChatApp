139298f0e543206840b363ba82ff45b4e50fd087
Shruthika-23 <166791474+Shruthika-23@users.noreply.github.com>
2025-08-06 15:32:32 +0530

    index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: "*"
+  }
+});
+
+io.on('connection', socket => {
+  console.log('User connected:', socket.id);
+
+  socket.on('sendMessage', (message) => {
+    io.emit('receiveMessage', message);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
